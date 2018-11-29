bca65f8cd7f10c197f725a14dbfcc1a80406ac19
Jarrett Bierman <biermaja1@pascack.org>
2018-11-29 09:50:29 -0500

    just a test

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,8 @@
-p5.disableFriendlyErrors = true; // disables FES
+//p5.disableFriendlyErrors = true; // disables FES
 var socket = io.connect('https://hills-connection.herokuapp.com/');
 //var socket = io.connect('localhost:3000');
 var r, g, b, size;
+var color;
 var alreadyDrawn = [];
 
 window.onload = function(){
@@ -12,7 +13,8 @@ window.onload = function(){
 function setup() {
     var canvas =  createCanvas(windowWidth, windowHeight);
     canvas.position(450, 0);
-    
+    frameRate(10);
+    color = '#ff0000';
     r = 255;
     g = 0;
     b = 0;
@@ -20,6 +22,7 @@ function setup() {
     background(51);
     
     socket.on('load-in', function(data){  
+        console.log(data.spots.color);
         alreadyDrawn = data.spots;
         console.log("first")
     });
@@ -29,20 +32,23 @@ function setup() {
         var spotLength = alreadyDrawn.length;
         console.log("second");
         for(var i = 0; i < spotLength; i++){
-            drawSpot(alreadyDrawn[i].x, alreadyDrawn[i].y, alreadyDrawn[i].s, alreadyDrawn[i].r, alreadyDrawn[i].g, alreadyDrawn[i].b);
+            drawSpot(alreadyDrawn[i].x, alreadyDrawn[i].y, alreadyDrawn[i].s, alreadyDrawn[i].color);
+            // drawSpot(alreadyDrawn[i].x, alreadyDrawn[i].y, alreadyDrawn[i].s, alreadyDrawn[i].r, alreadyDrawn[i].g, alreadyDrawn[i].b);
         }
     }, 500);
         
     
     //Where other people's drawings are.
     socket.on('orbs', function(data){
-        drawSpot(data.x, data.y, data.s, data.r, data.g, data.b);
+        drawSpot(data.x, data.y, data.s, data.color);
+        // drawSpot(data.x, data.y, data.s, data.r, data.g, data.b);
     });
     
     //erase the entire screen
     socket.on('delete', function(data){
         background(51);
-    });   
+    });  
+    
     
 }
 function draw(){}
@@ -50,14 +56,16 @@ function draw(){}
 function mouseDragged()
 {
     //send the information
-    socket.emit('orbs', {x: mouseX, y: mouseY, s: size, r: r, g: g, b: b});
-    drawSpot(mouseX, mouseY, size, r, g, b);
+    // socket.emit('orbs', {x: mouseX, y: mouseY, s: size, r: r, g: g, b: b});
+    socket.emit('orbs', {x: mouseX, y: mouseY, s: size, color: color});
+    drawSpot(mouseX, mouseY, size, color);
+    // drawSpot(mouseX, mouseY, size, r, g, b);
 }
 function mousePressed()
 {
     //send the information
-    socket.emit('orbs', {x: mouseX, y: mouseY, s: size, r: r, g: g, b: b});
-    drawSpot(mouseX, mouseY, size, r, g, b);
+    // socket.emit('orbs', {x: mouseX, y: mouseY, s: size, r: r, g: g, b: b});
+    // drawSpot(mouseX, mouseY, size, r, g, b);
 }
 
 //delete the screen
@@ -78,35 +86,39 @@ var yellow = document.getElementById('yellow');
 yellow.onclick = function(){r = 255;  g = 255;  b = 0;}
 
 var green = document.getElementById('green');
-green.onclick = function(){r = 0;  g = 255;  b = 0;}
+green.onclick = function(){color = '#008000';}
+// green.onclick = function(){r = 0;  g = 153;  b = 51;}
 
 var blue = document.getElementById('blue');
-blue.onclick = function(){r = 0;  g = 0;  b = 255;}
+blue.onclick = function(){color = '#0000ff';}
 
 var purple = document.getElementById('purple');
-purple.onclick = function(){r = 255;  g = 0;  b = 255;}
+purple.onclick = function(){color = '#800080';}
 
 var black = document.getElementById('black');
 black.onclick = function(){r = 0;  g = 0;  b = 0;}
 
-//Bigger and Smaller brush size buttons
-var bigger = document.getElementById('bigger');
-bigger.onclick = function(){
-    if(size < 100)
-    size += 5;
-}
-var smaller = document.getElementById('smaller');
-smaller.onclick = function(){
-    if(size > 5)
-    size -= 5;
+var white = document.getElementById('white');
+white.onclick = function(){r = 255;  g = 255;  b = 255;}
+
+var customChooser = document.getElementById('chooser');
+var custom = document.getElementById('custom');
+custom.onclick = function(){
+    custom.style.background = customChooser.value;
+    color = customChooser.value;
 }
 
-function drawSpot(x, y, s, r, g, b)
-{
-    fill(r, g, b);
-    noStroke();
-    ellipse(x, y, s, s);
+//Bigger and Smaller brush size buttons
+var slider = document.getElementById("range");
+var strokeSize = document.getElementById("size");
+strokeSize.innerHTML = slider.value;
+
+slider.oninput = function(){
+    size = (int)(this.value);
+    strokeSize.innerHTML = slider.value;
 }
+
+
 
 
 //THE CHAT STUFF
@@ -143,3 +155,16 @@ socket.on('chat', function(data){
     messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
 });
 
+// function drawSpot(x, y, s, r, g, b)
+// {
+//     fill(r, g, b);
+//     noStroke();
+//     ellipse(x, y, s, s);
+// }
+
+function drawSpot(x, y, s, c)
+{
+    fill(c);
+    noStroke();
+    ellipse(x, y, s, s);
+}
